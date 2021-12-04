198df7dcabf7bac354464e4acd202586f589132c
David Ruhmann <davidruhmann@gmail.com>
2021-12-03 20:29:48 -0500

    Update ubuntu-bedrock-arm-pack.sh

--- ubuntu-bedrock-arm-pack.sh
+++ ubuntu-bedrock-arm-pack.sh
@@ -3,6 +3,8 @@
 # Official Minecraft Bedrock Server Page: https://www.minecraft.net/en-us/download/server/bedrock
 # example: wget -qO - https://raw.githubusercontent.com/Ruhminations/nix-scripts/main/ubuntu-bedrock-arm-pack.sh | bash -s "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.1.02.zip"
 
+zip --help 2>/dev/null || echo "[error] zip required"
+curl --help 2>/dev/null || echo "[error] curl required"
 mkdir bedrock || echo "[error] bedrock folder already exists."
 pushd bedrock
 wget -O bedrock-server.zip $1
@@ -14,7 +16,7 @@ ldd bedrock_server | grep "=> /" | awk '{print $3}' | xargs -I '{}' cp -Lv '{}' 
 pushd depends
 cp -Lv /lib64/* ./
 zip -r bedrock_only_depends.zip *
-mv /* ../
+mv * ../
 popd
 rm -r depends
 curl --upload-file bedrock_only_depends.zip https://transfer.sh/bedrock_only_depends.zip
