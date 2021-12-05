89bdb68b63872042e04eb3323de0bda0c0bae024
David Ruhmann <davidruhmann@gmail.com>
2021-12-04 20:39:22 -0500

    Update oracle-cloud-ubuntu-s3fs-init.sh

--- oracle-cloud-ubuntu-s3fs-init.sh
+++ oracle-cloud-ubuntu-s3fs-init.sh
@@ -2,6 +2,6 @@
 # example: wget -qO - https://raw.githubusercontent.com/Ruhminations/nix-scripts/main/oracle-cloud-ubuntu-s3fs-init.sh | bash -s "user:pass" "bucket-name" "namespace" "region"
 
 sudo apt-get -y install s3fs nfs-kernel-server
-echo $1 > /etc/passwd-s3fs
+echo $1 | sudo tee /etc/passwd-s3fs
 chmod 600 /etc/passwd-s3fs
-echo "$2 /bedrock_bucket fuse.s3fs use_path_request_style,passwd_file=/root/.s3fs-password,url=https://$3.compat.objectstorage.$4.oraclecloud.com,endpoint=$4 kernel_cache,multipart_size=128,parallel_count=50,multireq_max=100,max_background=1000,_netdev" >> /etc/fstab
+echo "$2 /bedrock_bucket fuse.s3fs use_path_request_style,passwd_file=/root/.s3fs-password,url=https://$3.compat.objectstorage.$4.oraclecloud.com,endpoint=$4 kernel_cache,multipart_size=128,parallel_count=50,multireq_max=100,max_background=1000,_netdev" | sudo tee -a /etc/fstab
