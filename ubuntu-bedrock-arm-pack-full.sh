cf0a8d69f66297d22aaf2ba68803d223e8f77295
David Ruhmann <davidruhmann@gmail.com>
2022-01-08 10:19:07 -0500

    Create ubuntu-bedrock-arm-pack-full.sh

---
+++ ubuntu-bedrock-arm-pack-full.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+# docker run --rm -it ubuntu:20.04 /bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install zip unzip wget curl
+wget -qO - https://raw.githubusercontent.com/Ruhminations/nix-scripts/main/ubuntu-bedrock-arm-pack.sh | bash -s "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.2.03.zip"
