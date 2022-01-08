e2ec51c1933e3a9ecc1b2ed43769de702aef028a
David Ruhmann <davidruhmann@gmail.com>
2022-01-08 15:47:25 -0500

    Update ubuntu-bedrock-start.sh

--- ubuntu-bedrock-start.sh
+++ ubuntu-bedrock-start.sh
@@ -1,23 +1,24 @@
-#! /bin/sh
+#! /bin/bash
 
 if [ -z "$STY" ]; then
   # TODO pull ports from properties
   sudo iptables -L | grep 'ACCEPT.*udp.*19132' || sudo iptables -A INPUT -p udp --dport 19132 -j ACCEPT
   sudo iptables -L | grep 'ACCEPT.*udp.*19133' || sudo iptables -A INPUT -p udp --dport 19133 -j ACCEPT
 
-  sudo mkdir /mnt/bedrock
-  sudo mount -t tmpfs -o rw,size=2G tmpfs /mnt/bedrock
-  cp -r ./ /mnt/bedrock
-  pushd /mnt/bedrock
-
-  if [ -f /lib64/ld-linux-x86-64.so.2 ]; then
-    sudo rm /lib64/ld-linux-x86-64.so.2
-  fi
+  sudo rm /lib64/ld-linux-x86-64.so.2 || echo "no existing symlink"
   if [ -f ld-linux-x86-64.so.2 ]; then
     sudo ln -s ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2
   fi
-  exec screen -L -Logfile bedrock.log -dmS bedrock sh $0;
+
+  sudo mkdir /mnt/bedrock
+  sudo mount -t tmpfs -o rw,size=4G tmpfs /mnt/bedrock
+  cp -r ./ /mnt/bedrock
+  cd /mnt/bedrock
+
+  echo starting
+  exec screen -L -Logfile bedrock.log -dmS bedrock sh $0
 else
-  pushd /mnt/bedrock
+  echo hello
+  cd /mnt/bedrock
   LD_LIBRARY_PATH=. ./bedrock_server
 fi
