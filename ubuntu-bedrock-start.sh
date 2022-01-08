e94391b145ea160cd9ab8e8c3e218cb7b9d88f58
David Ruhmann <davidruhmann@gmail.com>
2022-01-08 10:53:20 -0500

    Update ubuntu-bedrock-start.sh

--- ubuntu-bedrock-start.sh
+++ ubuntu-bedrock-start.sh
@@ -2,9 +2,10 @@
 # example: 
 
 #if [ -z "$STY" ]; then exec screen -L -Logfile bedrock.log -dmS bedrock sh $0; fi
-if [ ! -f /lib64/ld-linux-x86-64.so.2 ] && [ -f ld-linux-x86-64.so.2 ]; then
+if [ -f /lib64/ld-linux-x86-64.so.2 ]; then
+  sudo rm /lib64/ld-linux-x86-64.so.2
+fi
+if [ -f ld-linux-x86-64.so.2 ]; then
   sudo ln -s ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2
-else
-  echo "[info] ld-linux-x86-64.so.2 mapping skipped."
 fi
 LD_LIBRARY_PATH=. ./bedrock_server
