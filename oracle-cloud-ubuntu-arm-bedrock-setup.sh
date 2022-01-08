82e944064b15a5b5696547ae04a2a9a5a2a03c18
David Ruhmann <davidruhmann@gmail.com>
2022-01-08 10:47:30 -0500

    Update oracle-cloud-ubuntu-arm-bedrock-setup.sh

--- oracle-cloud-ubuntu-arm-bedrock-setup.sh
+++ oracle-cloud-ubuntu-arm-bedrock-setup.sh
@@ -3,9 +3,15 @@
 
 wget -qO - https://raw.githubusercontent.com/Ruhminations/nix-scripts/main/ubuntu-bedrock-arm-pack.sh | bash -s "https://github.com/Ruhminations/nix-scripts/raw/main/ubuntu-arm-x86-init.sh"
 mkdir bedrock || { echo "[error] bedrock folder already exists." && exit 3; }
-pushd bedrock
+cd bedrock
 wget -O bedrock-server.zip $1 || { echo "[error] failed to download the bedrock server zip; please verify the url." && exit 4; }
 unzip -o bedrock-server.zip || { echo "[error] zip downloaded failed to extract, please validate the url's file." && exit 5; }
 wget -O bedrock-depends.zip $2 || { echo "[error] failed to download the bedrock depends zip; please verify the url." && exit 6; }
 unzip -o bedrock-depends.zip || { echo "[error] zip downloaded failed to extract, please validate the url's file." && exit 7; }
-echo "Done"
+wget -O bedrock-start.sh || { echo "[error] failed to download the bedrock start script; please verify the url." && exit 8; }
+sudo mkdir /mnt/bedrock
+sudo mount -t tmpfs -o rw,size=2G tmpfs /mnt/bedrock
+cp -r ./ /mnt/bedrock
+cd /mnt/bedrock
+ls
+echo "ready to run '/mnt/bedrock/bedrock-start.sh'"
