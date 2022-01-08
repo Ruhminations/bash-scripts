6064fc482e04000d8fe52b6981f597d527305a3e
David Ruhmann <davidruhmann@gmail.com>
2022-01-08 10:37:45 -0500

    Update and rename ubuntu-bedrock-arm-pack-full.sh to docker-ubuntu-bedrock-arm-pack.sh

--- docker-ubuntu-bedrock-arm-pack.sh
+++ docker-ubuntu-bedrock-arm-pack.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # docker run --rm -it -u root ubuntu:20.04 /bin/bash
 
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install zip unzip wget curl gawk
+apt-get update
+apt-get -y upgrade
+apt-get -y install zip unzip wget curl gawk
 wget -qO - https://raw.githubusercontent.com/Ruhminations/nix-scripts/main/ubuntu-bedrock-arm-pack.sh | bash -s "https://minecraft.azureedge.net/bin-linux/bedrock-server-1.18.2.03.zip"
